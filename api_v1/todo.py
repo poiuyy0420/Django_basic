060b10b503440e8c2ca0daf275b1556a8f26b3b7
poiuyy0420 <poiuyy0420@gmail.com>
2021-01-21 00:24:25 +0900

    slack TODO 생성/리스트

--- api_v1/todo.py
+++ api_v1/todo.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask import request
 from flask import Blueprint
+from models import Todo, db
 import requests
 from . import api
 
@@ -20,8 +21,30 @@ def todos():
 
 
 
-@api.route('/test', methods=['POST'])
-def test():
-    res = request.form['text']
-    print(res)
-    return jsonify(res)
+@api.route('/slack/todos', methods=['POST'])
+def slack_todos():
+    # 명령어 구분 띄어쓰기
+    # /flasktodo create aaaa
+    # /flasktodo list
+    res = request.form['text'].split(' ')
+    # 첫번째 변수는 cmd, 나머지는 *args
+    cmd, *args = res
+
+    ret_msg = ''
+    if cmd == 'create':
+        todo_name = args[0]
+
+        todo = Todo()
+        todo.title = todo_name
+
+        db.session.add(todo)
+        db.session.commit()
+        ret_msg = 'todo가 생성되었습니다'
+
+    elif cmd == 'list':
+        todos = Todo.query.all()
+        for idx, todo in enumerate(todos):
+            ret_msg += '%d. %s (~ %s)\n'%(idx+1, todo.title, str(todo.tstamp))
+        
+
+    return ret_msg
--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+import os
 from flask import Flask
 from flask import request, redirect, render_template, session
 from flask import Blueprint
+from models import db
 from api_v1 import api as api_v1
 
 app = Flask(__name__)
 app.register_blueprint(api_v1, url_prefix='/api/v1')
 
+basdir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basdir, 'db.aplite')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'jqiowejrojzxcovnklqnweiorjqwoijroi'
+
+db.init_app(app)
+db.app = app
+db.create_all()
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
